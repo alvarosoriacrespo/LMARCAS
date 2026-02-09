116cdf1885876e17e714db9bcc66961659ca9fc0
Alvaro <alvsorcre@client172.com>
2026-02-09 11:44:39 +0100

    basilisco

--- java/js.app.js
+++ java/js.app.js
@@ -2,3 +2,7 @@
       console.log("Adios")
       console.error("Error: no se puede conectar al servidor")
       console.warn("OJO")
+
+
+      let nombre= "basilisco"
+      console.log("Te llamas:"+ nombre)
