7dc3c10fb47f8ec594535a1852dcda2561d2e798
Alvaro <alvsorcre@client172.com>
2026-02-25 11:53:22 +0100

    3/4 de ej

--- java/js.app.js
+++ java/js.app.js
@@ -107,6 +107,9 @@ return Math.floor(Math.random() * (max - min + 1)) + min
 
 let num=numeroAleatorio(1,99)
 let letraespecial=Especiales[numeroAleatorio(0,30)]
-let password=num + letraespecial
+let caracter= caracteres[numeroAleatorio(0,26)]
+let mayu=Mayusculas[numeroAleatorio(1,26)]
+let password=letraespecial+num+caracter+mayu
+      
 
 console.log("La contraseña es: " + password)
