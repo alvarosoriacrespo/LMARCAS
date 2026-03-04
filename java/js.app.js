1dd97dfe1452576c2ad311468bc5a94a2e76e059
Alvaro <alvsorcre@client172.com>
2026-03-04 10:24:52 +0100

    recuperando

--- java/js.app.js
+++ java/js.app.js
@@ -20,8 +20,20 @@
       
 //contenedor.innerText = "La contraseña es: " + password;
 
+//VARIABLES
 let numero=document.getElementById("numero1")
 let numero2=document.getElementById("numero2")
 let solucion=document.getElementById("solucion")
 
 let run=document.getElementById("run")
+
+//EVENTOS
+run.addEventListener("click", (e) => {
+      let num=(numero.value)
+      let num2=(numero2.value)
+      let sol= parseInt(num) + parseInt(num2)
+      console.log(sol)
+      solucion.innerHTML=sol
+}
+
+)
