c59fd74472a8be5edbcb7b0cf00c3e9973dd81d0
Alvaro <alvsorcre@client172.com>
2026-03-04 11:42:21 +0100

    cadena en vez de suma

--- java/js.app.js
+++ java/js.app.js
@@ -29,11 +29,18 @@ let run=document.getElementById("run")
 
 //EVENTOS
 run.addEventListener("click", (e) => {
-      let num=(numero.value)
-      let num2=(numero2.value)
-      let sol= parseInt(num) + parseInt(num2)
-      console.log(sol)
-      solucion.innerHTML=sol
+      let num= (numero.value)
+      let num2= (numero2.value)
+      let cadenaTotal=""
+      for (let i=0;i<num2;i++){
+            cadenaTotal=cadenaTotal + num + "<BR>"
+            console.log("i vale: " + i + "<->" + num)
+      }
+      solucion.innerHTML=cadenaTotal
+
+      //let sol= parseInt(num) + parseInt(num2)
+      //console.log(sol)
+      //solucion.innerHTML=sol
 }
 
 )
