20dbc12dc54c73cd72ae8b06ec050a848696a865
Alvaro <alvsorcre@client172.com>
2026-03-09 11:49:50 +0100

    jss

--- 4ejs js basicos/js.app.js
+++ 4ejs js basicos/js.app.js
@@ -52,10 +52,11 @@ function numeroAleatorio(min, max) {
 return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+
 let num=numeroAleatorio(1,99)
 let letraespecial=Especiales[numeroAleatorio(0,30)]
 let caracter= caracteres[numeroAleatorio(0,26)]
 let mayu=Mayusculas[numeroAleatorio(1,26)]
 let password=letraespecial+num+caracter+mayu
 //console.log(password)
-      
+
